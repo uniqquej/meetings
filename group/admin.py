4a07a6a8338a6fb8575875efa1cb370a94bdd53f
uniqquej <cjy4477@gmail.com>
2023-10-14 07:13:07 +0900

    feat: admin 추가

--- group/admin.py
+++ group/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from group.models import Group, Meeting
+
+admin.site.register(Group)
+admin.site.register(Meeting)
--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from post.models import Post, Recruitment, Comment
+
+admin.site.register(Post)
+admin.site.register(Recruitment)
+admin.site.register(Comment)
+
+
